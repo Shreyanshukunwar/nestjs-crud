bc5446410e62068365d9b23430ed8a97ad5b5ada
shreyanshu <shreyanshukunwar@gmail.com>
2021-08-12 16:56:00 +0545

    first commit

---
+++ src/products/products.service.ts
@@ -0,0 +1,69 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { Product } from './product.model';
+
+@Injectable()
+export class ProductsService {
+  private products: Product[] = [];
+
+  constructor(
+    @InjectModel('Product') private readonly productModel: Model<Product>,
+  ) {}
+
+  async insertProduct(title: string, desc: string, price: number) {
+    const newProduct = new this.productModel({
+      title,
+      description: desc,
+      price,
+    });
+    const result = await newProduct.save();
+    console.log('Result:', result);
+    return result.id as string;
+  }
+
+  async getProducts() {
+    const products = await this.productModel.find().exec();
+    return products.map((prod) => ({
+      id: prod.id,
+      title: prod.title,
+      description: prod.desc,
+      price: prod.price,
+    }));
+  }
+
+  getSingleProduct(productId: string) {
+    const product = this.findProduct(productId)[0];
+    return { ...product };
+  }
+
+  updateProduct(productId: string, title: string, desc: string, price: number) {
+    const [product, index] = this.findProduct(productId);
+    const updatedProduct = { ...product };
+    if (title) {
+      updatedProduct.title = title;
+    }
+    if (desc) {
+      updatedProduct.desc = desc;
+    }
+    if (price) {
+      updatedProduct.price = price;
+    }
+    this.products[index] = updatedProduct;
+  }
+
+  deleteProduct(prodId: string) {
+    const [_, index] = this.findProduct(prodId);
+    this.products.splice(index, 1);
+  }
+
+  private findProduct(id: string): [Product, number] {
+    const productIndex = this.products.findIndex((prod) => prod.id === id);
+    const product = this.products[productIndex];
+    if (!product) {
+      throw new NotFoundException('Could not find Product');
+    }
+    return [product, productIndex];
+  }
+}
